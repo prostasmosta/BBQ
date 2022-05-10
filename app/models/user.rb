70525019fa941a33622c510c570e705b7514f036
prostasmosta <prostasmosta@gmail.com>
2022-05-10 10:36:49 +0300

    devise, new links on index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,16 @@
 class User < ActiveRecord::Base
-  EMAIL_VAL = /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   has_many :events
 
   validates :name, presence: true, length: {maximum: 35}
-  validates :email, presence: true, length: {maximum: 255}
-  validates :email, uniqueness: true
-  validates :email, format: EMAIL_VAL
+
+  before_validation :set_name, on: :create
+
+  private
+
+  def set_name
+    self.name = "User #{rand(777)}" if self.name.blank?
+  end
 end
--- db/migrate/20220509181340_add_devise_to_users.rb
+++ db/migrate/20220509181340_add_devise_to_users.rb
@@ -1,7 +1,7 @@
 class AddDeviseToUsers < ActiveRecord::Migration[7.0]
   def self.up
     change_table :users do |t|
-      t.string :email,              null: false, default: ""
+      t.change :email, :string, null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       t.string   :reset_password_token
@@ -10,7 +10,7 @@ class AddDeviseToUsers < ActiveRecord::Migration[7.0]
       t.datetime :remember_created_at
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :email, unique: true
     add_index :users, :reset_password_token, unique: true
   end
 
