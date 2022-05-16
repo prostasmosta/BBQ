4fda05f00c5236800e3b42d1422f68401e90793c
prostasmosta <prostasmosta@gmail.com>
2022-05-16 09:51:09 +0300

    mailjet fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.action_mailer.raise_delivery_errors = true
+
   # Enable server timing
   config.server_timing = true
 
