10b5992a227adcac7e7778ddf9ed5b1dbf12b406
prostasmosta <prostasmosta@gmail.com>
2022-05-16 13:35:31 +0300

    prod mailjet

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: '587',
-    user_name: ENV['MAILJET_SENDER'],
+    user_name: ENV['MAILJET_USERNAME'],
     password: ENV['MAILJET_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
