24de74a6b3cf6c9c0880dddd9e34d063afe118a5
prostasmosta <prostasmosta@gmail.com>
2022-05-19 09:51:24 +0300

    subscribers validations fixes

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -4,12 +4,17 @@ class Subscription < ApplicationRecord
   belongs_to :event
   belongs_to :user, optional: true
 
-  validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: EMAIL_REGEXP, unless: -> { user.present? }
-  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
-  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
-  validate :same_user
-  validate :existing_email, unless: -> { user.present? }
+  with_options if: -> { user.present? } do
+    validates :user, uniqueness: {scope: :event_id}
+    validate :same_user
+  end
+
+  with_options unless: -> { user.present? } do
+    validates :user_name, presence: true
+    validates :user_email, presence: true, format: EMAIL_REGEXP
+    validates :user_email, uniqueness: {scope: :event_id}
+    validate :existing_email
+  end
 
   def user_name
     user&.name || super
@@ -19,11 +24,13 @@ class Subscription < ApplicationRecord
     user&.email || super
   end
 
+  private
+
   def same_user
-    errors.add(self.user.name, I18n.t('controllers.subscriptions.error')) if user == event.user
+    errors.add(:user_email, :same_user) if user == event.user
   end
 
   def existing_email
-    errors.add(:user_email, I18n.t('controllers.subscriptions.error')) if User.exists?(email: user_email)
+    errors.add(:user_email, :existing_email) if User.exists?(email: user_email)
   end
 end
