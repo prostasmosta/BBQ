bd3eebd1957f5133ec2f07bee158a42371eb4041
prostasmosta <prostasmosta@gmail.com>
2022-06-03 16:50:32 +0300

    small fixes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -43,8 +43,8 @@ class CommentsController < ApplicationController
   end
 
   def notify_subscribers(event, comment)
-      all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [comment.user&.email]).uniq
-      all_emails.each do |mail|
+    all_emails = (event.subscriptions.map(&:user_email) + [event.user.email] - [comment.user&.email]).uniq
+    all_emails.each do |mail|
       EventMailer.comment(comment, mail).deliver_now
     end
   end
